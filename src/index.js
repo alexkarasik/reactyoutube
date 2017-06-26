a2db3040fffdf03355b9f3ea6d62c84c960b1a3a
kinkorea <alexkarasik89@gmail.com>
2017-06-26 13:25:21 -0400

    Add a search bar that changes state based on input

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyAT4oXalnapl5GFdbdrqYRaDe5zOsDiLD4';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
